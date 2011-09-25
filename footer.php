1ffd843f71c7d14f66282acde51f629175884bb4
Zane M. Kolnik <zanematthew@gmail.com>
2011-09-25 02:00:29 -0400

    start of theme

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div class="container_12">
+
+	<div class="grid_12">
+		<footer>
+			<div class="grid_5">
+				<h2>TaskTracker</h2>
+			</div>
+
+		</footer>
+
+	</div>
+
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php get_header(); ?>
+<div class="container_12">
+
+	<div class="grid_12">
+		<div class="main-container">
+		<div class="attention-container">		
+			<h1 class="title">TaskTracker</h1>				
+			<h2 class="slogan">Introducing the TaskTracker: Now you can quickly add your Tasks and have fun doing it as well.</h2>			
+		</div>
+			
+		<div class="callout-container">
+			<div class="callout">
+				<h3>Sign-up</h3>
+				<h4>How fast can I sign-up.</h4>
+				<p>Lorem ipsum dolor sit amet, praesent at pertineo vel, vindico mos nunc, eros. Eum et vulpes proprius genitus, nutus.</p>
+				<button class="large" id="signup_botton">SIGN-UP</button>
+			</div>
+		
+			<div class="callout">
+				<h3>Demo</h3>
+				<h4>Okay, let me check it out.</h4>
+				<p>Accumsan ulciscor minim consectetuer, tua plaga nunc, tum roto pertineo, inhibeo ex facilisi tincidunt duis facilisi ibidem. </p>
+				<button class="large" id="demo_button">DEMO</button>
+			</div>
+
+			<div class="callout screenshot">	
+				<h3>Screenshots</h3>
+				<h4>So what does it look like?</h4>
+				<p>Pertineo, inhibeo ex facilisi tincidunt duis facilisi ibide. Lorem ipsum dolor sit amet, praesent at pertineo vel, vindico mos nunc, eros. Eum et vulpes proprius genitus, nutus.</p>
+				<ul>
+					<li><a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/d1.png"/></a></li>
+					<li><a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/d2.png"/></a></li>
+					<li><a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/d3.png"/></a></li>
+					<li><a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/d4.png"/></a></li>			
+					<li><a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/d2.png"/></a></li>					
+				</ul>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
