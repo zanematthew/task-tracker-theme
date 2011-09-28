4da582bfba58546e4b8d4ee31fe71fd1f5c44f5b
Zane M. Kolnik <zanematthew@gmail.com>
2011-09-28 02:54:55 -0400

    changed id to class

--- header.php
+++ header.php
@@ -58,8 +58,8 @@
 	<div class="grid_12">
 		<div class="grid_6 alpha">
 			<hgroup>
-				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
-				<h2 id="site-description"><span>&ndash;</span><?php bloginfo( 'description' ); ?></h2>
+				<h1 class="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
+				<h2 class="site-description"><span>&ndash;</span><?php bloginfo( 'description' ); ?></h2>
 			</hgroup>
 		</div>
 		<div class="grid_6 omega right">
