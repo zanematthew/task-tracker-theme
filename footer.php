957d3f2bec53189e7947a5fb0098a54d35e107b6
Zane M. Kolnik <zanematthew@gmail.com>
2011-10-19 09:57:00 -0700

    idk

--- footer.php
+++ footer.php
@@ -2,10 +2,12 @@
 	<div class="grid_12">
 		<footer class="primary">
 			<div class="grid_5 alpha">
+				<!--
 				<hgroup>
 					<h3 class="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
 					<h4 class="site-description"><span>&ndash;</span><?php bloginfo( 'description' ); ?></h2>
 				</hgroup>				
+				-->
 			</div>
 			<div class="grid_5 push_2 omega">
 				<nav>
